860dd2a9f75a107c8776897c43b9312b1daa6cef
Thiago <thiago.romaris@sptech.school>
2023-05-12 14:50:13 -0300

    API Aplicada

---
+++ site/public/js/funcoes.js
@@ -0,0 +1,57 @@
+// sessão
+function validarSessao() {
+    // aguardar();
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    const nomeSaudacao = document.getElementById("nome_saudacao");
+    const emailSaudacao = document.getElementById("email_saudacao");
+
+    if (email != null && nome != null) {
+        // window.alert(`Seja bem-vindo, ${nome}!`);
+        nomeSaudacao.innerHTML = `${nome}`;
+        emailSaudacao.innerHTML = `${email}`
+
+        // finalizarAguardar();
+    } else {
+        window.location = "../Page Login/paginalogin.html";
+    }
+}
+
+function limparSessao() {
+    // aguardar();
+    sessionStorage.clear();
+    // finalizarAguardar();
+    window.location = "../Page Login/paginalogin.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
+
+// modal
+function mostrarModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "flex";
+}
+
+function fecharModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "none";
+}
+
